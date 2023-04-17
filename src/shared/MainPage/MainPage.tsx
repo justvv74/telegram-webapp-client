8b2512dee7dcaf927f0c46b7c51bde3eefc0f4b1
just_v <just_v@inbox.ru>
2023-04-18 02:04:21 +0500

    fix7

--- src/shared/MainPage/MainPage.tsx
+++ src/shared/MainPage/MainPage.tsx
@@ -28,7 +28,7 @@ export function MainPage() {
 
     axios
       .post(
-        `https://telegram-webapp-server-kh82y3j77-justvv74.vercel.app/search`,
+        `https://telegram-webapp-server.vercel.app/search`,
         {
           userId: tg?.initDataUnsafe?.user?.id || "1308147330",
           tag: value,
@@ -52,9 +52,7 @@ export function MainPage() {
 
   function test() {
     axios
-      .get(
-        `https://telegram-webapp-server-kh82y3j77-justvv74.vercel.app:3000/test`
-      )
+      .get(`https://telegram-webapp-server.vercel.app:3000/test`)
       .then((res) => console.log(res))
       .catch((err) => console.log(err));
   }
